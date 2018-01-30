45cc730bdebe37f0427b59096cc278973ea2d04b
raphaeltimbo <raphaelts@gmail.com>
2018-01-30 18:33:35 -0200

    Added files for jupyter.

---
+++ jupyter/custom.js
@@ -0,0 +1,10 @@
+// Configure CodeMirror
+require([
+  'nbextensions/vim_binding/vim_binding',   // depends your installation
+], function() {
+  // Map jj to <Esc>
+  CodeMirror.Vim.map("jj", "<Esc>", "insert");
+  CodeMirror.Vim.map("-", "$", "normal");
+  CodeMirror.Vim.map("-", "$", "visual");
+});
+
